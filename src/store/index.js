dbc4c50e86ec54e974be8d390dd84b3ac4942bf6
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-16 16:43:21 +0200

    main

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -13,7 +15,7 @@ const stringMiddleware = () => (next) => (action) => {
 
 const store = createStore( 
                     combineReducers({heroes, filters}),
-                    compose(applyMiddleware(stringMiddleware),
+                    compose(applyMiddleware(ReduxThunk, stringMiddleware),
                             window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
                     );
 
