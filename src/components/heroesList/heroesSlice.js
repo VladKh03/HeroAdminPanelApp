e4bf10b6d5bc6858caa8b830b680f97dbaba31c7
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-16 19:09:58 +0200

    main

--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,41 +1,28 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import {useHttp} from '../../hooks/http.hook';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 }
 
-export const fetchHeroes = createAsyncThunk(
-    'heroes/fetchHeroes',
-    async () => {
-        const {request} = useHttp();
-        return await request("http://localhost:3001/heroes");
-    }
-);
-
 const heroesSlice = createSlice({
     name: 'heroes',
     initialState,
     reducers: {
+        heroesFetching: state => {state.heroesLoadingStatus = 'loading'},
+        heroesFetched: (state, action) => {
+            state.heroesLoadingStatus = 'idle';
+            state.heroes = action.payload;
+        },
+        heroesFetchingError: state => {
+            state.heroesLoadingStatus = 'error';
+        },
         heroCreated: (state, action) => {
             state.heroes.push(action.payload);
         },
         heroDeleted: (state, action) => {
             state.heroes = state.heroes.filter(item => item.id !== action.payload);
         }
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
-            .addCase(fetchHeroes.fulfilled, (state, action) => {
-                state.heroesLoadingStatus = 'idle';
-                state.heroes = action.payload;
-            })
-            .addCase(fetchHeroes.rejected, state => {
-                state.heroesLoadingStatus = 'error';
-            })
-            .addDefaultCase(() => {})
     }
 });
 
