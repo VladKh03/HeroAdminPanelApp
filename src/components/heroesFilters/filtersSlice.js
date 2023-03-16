828c9a10a0a38166e5b4e9d690d565b1de1e926a
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-16 20:38:10 +0200

    main

--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -1,25 +1,25 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter } from "@reduxjs/toolkit";
 import {useHttp} from '../../hooks/http.hook';
 
-const initialState = {
-    filters: [],
+const filtersAdapter = createEntityAdapter();
+
+const initialState = filtersAdapter.getInitialState({
     filtersLoadingStatus: 'idle',
     activeFilter: 'all'
-}
+});
 
 export const fetchFilters = createAsyncThunk(
     'filters/fetchFilters',
-    () => {
+    async () => {
         const {request} = useHttp();
-        return request("http://localhost:3001/filters")
+        return await request("http://localhost:3001/filters");
     }
-)
+);
 
 const filtersSlice = createSlice({
     name: 'filters',
     initialState,
     reducers: {
-
         filtersChanged: (state, action) => {
             state.activeFilter = action.payload;
         }
@@ -29,10 +29,10 @@ const filtersSlice = createSlice({
             .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
             .addCase(fetchFilters.fulfilled, (state, action) => {
                 state.filtersLoadingStatus = 'idle';
-                state.filters = action.payload;
+                filtersAdapter.setAll(state, action.payload);
             })
-            .addCase(fetchFilters.rejected, (state, action) => {
-                state.activeFilter = action.payload;
+            .addCase(fetchFilters.rejected, state => {
+                state.filtersLoadingStatus = 'error';
             })
             .addDefaultCase(() => {})
     }
@@ -41,6 +41,9 @@ const filtersSlice = createSlice({
 const {actions, reducer} = filtersSlice;
 
 export default reducer;
+
+export const {selectAll} = filtersAdapter.getSelectors(state => state.filters);
+
 export const {
     filtersFetching,
     filtersFetched,
