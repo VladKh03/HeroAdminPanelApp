3fff8c235b9b703340313ebd8087e13d3210d207
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-16 18:09:29 +0200

    main

---
+++ src/components/heroesList/heroesSlice.js
@@ -0,0 +1,14 @@
+// import  {createSlice} from "@reduxjs/toolkit"
+
+// const initialState = {
+//     heroes: [],
+//     heroesLoadingStatus: 'idle',
+// }
+
+// const heroesSlice = createSlice({
+//     name: 'heroes',
+//     initialState,
+//     reducers: {
+//         heroesFetching: 
+//     }
+// })
