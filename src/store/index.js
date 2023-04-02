27a95c233a430b9d135dcb2edb0217681961c90f
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-04-02 15:39:53 +0300

    main

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../components/heroesList/heroesSlice';
-
 import filters from '../components/heroesFilters/filtersSlice';
 
 const stringMiddleware = () => (next) => (action) => {
